437e7670bfa5dc03cad44b46846e5600e87aa7ba
SaulLawliet <october.sunbathe@gmail.com>
2026-09-01 23:28:42 +0800

    🎲 Transpiled mainframe.

--- src/danjuanfunds.py
+++ src/danjuanfunds.py
@@ -17,29 +17,36 @@ session.headers.update({"User-Agent": UserAgent().random})
 
 def main():
     funds = get_data(ENV_KEY, MOCK_CONFIG)
-    if funds:
-        for fund in funds:
-            print(fund)
-            resp = session.get(f"https://danjuanfunds.com/djapi/fund/nav/history/{fund.get('code')}?page=1&size=3")
-            if resp.status_code == 200:
-                items = resp.json().get("data", {}).get("items", [])
-                if items:
-                    if items[0].get("date") == fund.get("last_update"):
-                        print(f"基金 {fund.get('code')} 无更新, 最新净值日期: {items[0].get('date')}")
-                    else:
-                        print(f"基金 {fund.get('code')} 有更新, 最新净值日期: {items[0].get('date')}")
-                        fund["last_update"] = items[0].get("date")
-                        content = f"基金: {fund.get('code')}, 本金: {fund.get('principal')}\n"
-                        for item in items:
-                            now = float(item.get("nav")) * fund.get("share")
-                            profit = now - fund.get("principal")
-                            profit_margin = ((100 * profit) / fund.get("principal")) if fund.get("principal") else 0
+    if not funds:
+        print("未检测到环境变量, 跳过!")
+        return
 
-                            content += f"\n{item.get('date')}: {now:.2f} ({profit:.2f} / {profit_margin:.2f}%)"
+    for fund in funds:
+        print(fund)
 
-                        notify_and_save(ENV_KEY, funds, "雪球基金", content)
-    else:
-        print("\n💡[提示]未检测到环境变量, 跳过!\n")
+        resp = session.get(f"https://danjuanfunds.com/djapi/fund/nav/history/{fund.get('code')}?page=1&size=3")
+        if resp.status_code != 200:
+            print(f"基金 {fund.get('code')}, 请求失败, 状态码: {resp.status_code}")
+            continue
+
+        items = resp.json().get("data", {}).get("items", [])
+        if items:
+            # item: {'date': '2026-08-31', 'nav': '5.6000', 'percentage': '-0.28', 'value': '5.6000'}
+            last_update = items[0].get("date")
+            if last_update == fund.get("last_update"):
+                print(f"基金 {fund.get('code')} 无更新, 最新净值日期: {last_update}")
+            else:
+                print(f"基金 {fund.get('code')} 有更新, 最新净值日期: {last_update}")
+                fund["last_update"] = last_update
+                content = f"基金: {fund.get('code')}, 本金: {fund.get('principal')}\n"
+                for item in items:
+                    now = float(item.get("nav")) * fund.get("share")
+                    profit = now - fund.get("principal")
+                    profit_margin = ((100 * profit) / fund.get("principal")) if fund.get("principal") else 0
+
+                    content += f"\n{item.get('date')}: {now:.2f} ({profit:.2f} / {profit_margin:.2f}%)"
+
+                notify_and_save(ENV_KEY, funds, "雪球基金", content)
 
 
 if __name__ == "__main__":
